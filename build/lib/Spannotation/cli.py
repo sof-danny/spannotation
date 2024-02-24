7a1cd0667ddd5b234676a1bac587c9e277c8e166
Folorunsho Samuel <77319818+sof-danny@users.noreply.github.com>
2024-02-24 11:57:27 -0600

    Update cli.py

--- build/lib/Spannotation/cli.py
+++ build/lib/Spannotation/cli.py
@@ -1,11 +1,25 @@
 from Spannotation.mask_generator import MaskGenerator
 
-def main():
+def process_single_image(generator):
+    image_path = input("Enter the path to your image: ")
+    save_path = input("Enter the path to save the mask: ")
+    generator.process_image(image_path, save_path)
+
+def process_image_folder(generator):
     folder_path = input("Enter the path to your images folder: ")
     save_path = input("Enter the path to save masks: ")
-
-    generator = MaskGenerator()
     generator.process_folder(folder_path, save_path)
+
+def main():
+    generator = MaskGenerator()
+    choice = input("Do you want to process a single image (1) or a folder (2)? [1/2]: ")
+
+    if choice == '1':
+        process_single_image(generator)
+    elif choice == '2':
+        process_image_folder(generator)
+    else:
+        print("Invalid choice. Please enter 1 or 2.")
 
 if __name__ == "__main__":
     main()
